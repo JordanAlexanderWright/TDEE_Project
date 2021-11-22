75e8d208b38f5a78ae84eca0440264f0f5adef79
Soule222 <50902734+Soule222@users.noreply.github.com>
2021-11-22 15:25:28 -0500

    Update api_choice.py
    Working on being able to create a listbox that will display the users choice.
    
    listboxname.get(ANCHOR) will get the selected god damn input. How you are supposed to know that without google fu, hell if I know.

--- api_choice.py
+++ api_choice.py
@@ -1,27 +1,60 @@
 import requests
+from tkinter import *
+from tkinter import ttk
 
-print("Please Input a food you'd like to lookup")
-food = input()
-my_key = 'WsAc8bOO00nv2sgDqa7PkIYHZtz2Utq8tKJHVuKS'
-query_type = 'list'
-payload = {'api_key': my_key, 'query': food}
-response = requests.get("https://api.nal.usda.gov/fdc/v1/foods/list", params=payload)
+class Window(Tk):
 
-print(response.status_code)
+    def __init__(self):
+        super().__init__()
 
-information = response.json()
+        self.title('Food Selection')
+        self.minsize(800, 800)
+        self.create_entry()
+
+    def create_entry(self):
+        food_var = StringVar()
+        food_search = Entry(self, bd=10, textvariable=food_var, width=40)
+        food_search.pack()
+
+        food_submit = Button(self, bd=10, text='Search', command=lambda: print("I was clicked"))
+        food_submit.pack()
+
+        api_list = StringVar()
+        api_list.set('Strawberry1 Strawberry2 Strawberry3')
+        food_choices = Listbox(self, height=10, listvariable=api_list)
+        food_choices.pack()
+
+        print(type(api_list.get()))
+        food_choice_sub = Button(self, bd=10, text='Yum!!', command=lambda: print(food_choices.get(ANCHOR)))
+        food_choice_sub.pack()
+#
+# print("Please Input a food you'd like to lookup")
+# food = input()
+# my_key = 'WsAc8bOO00nv2sgDqa7PkIYHZtz2Utq8tKJHVuKS'
+# query_type = 'list'
+# payload = {'api_key': my_key, 'query': food}
+# response = requests.get("https://api.nal.usda.gov/fdc/v1/foods/list", params=payload)
+#
+# print(response.status_code)
+#
+# information = response.json()
 
 # response.json() here creates a list of dictionaries.
-
-for item in information:
-    try:
-        print(item['fdcId', 'description'])
-    except KeyError:
-        print("There may be an error")
-
-# print(information)
+#
+# keys = ['fdcId', 'description', 'dataType', 'brandOwner']
+# for item in information:
+#     for key in keys:
+#         try:
+#             print(item[key])
+#         except KeyError:
+#             print(f"There may be an error with this key:{key}")
 #
 #
+#
+
+window = Window()
+
+window.mainloop()
 # fdcId
 # description
 # dataType
